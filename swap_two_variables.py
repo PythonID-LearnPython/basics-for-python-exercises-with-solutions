f9050843ec1dba0198e7645ac506fc1cbf6c5bd9
PythonID-LearnPython <tranhuudoan0804@gmail.com>
2024-12-13 09:32:23 +0700

    Add files via upload

---
+++ swap_two_variables.py
@@ -0,0 +1,12 @@
+# Lesson 2: Python program to swap two variables
+
+x = 8
+y = 13
+
+# create a temporary variable and swap the values
+temp = x
+x = y
+y = temp
+
+print('The value of x after swapping: {}'.format(x))
+print('The value of y after swapping: {}'.format(y))
